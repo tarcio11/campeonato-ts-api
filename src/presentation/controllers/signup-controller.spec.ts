c0f81e3fbbf20a2c5aac8be62c65f155b3acf03f
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-23 13:02:10 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup-controller'
+
+import faker from 'faker'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', async () => {
+    const sut = new SignUpController()
+    const request = {
+      body: {
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+    }
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
