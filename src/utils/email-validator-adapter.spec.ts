8f3dc783b9c67822904e7920466a6308cb8fdabc
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-24 13:02:25 -0300

    refactor: move sut creation to a factory method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -2,9 +2,13 @@ import { EmailValidatorAdapter } from './email-validator-adapter'
 
 import faker from 'faker'
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid(faker.internet.email())
     expect(isValid).toBe(false)
   })
