402a93bd6c3bc306864a5cf76d2304a74d2f2f8f
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-28 13:26:26 -0300

    feat: ensure DbAddAccount returns true on success

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -9,7 +9,7 @@ export class DbAddAccount implements AddAccount {
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
     const hashedPassword = await this.hasher.hash(accountData.password)
-    await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
-    return null
+    const isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    return isValid
   }
 }
