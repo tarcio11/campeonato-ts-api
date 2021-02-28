8724ad891b8d069cd28530e59289f23b3dd0ebcd
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-28 12:08:21 -0300

    feat: ensure DbAddAccount calls Hasher with correct plaintext

---
+++ src/data/usecases/db-add-account.ts
@@ -0,0 +1,11 @@
+import { AddAccount } from '../../domain/usecases'
+import { Hasher } from '../protocols/cryptography/hasher'
+
+export class DbAddAccount implements AddAccount {
+  constructor (private readonly hasher: Hasher) {}
+
+  async add (account: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.hasher.hash(account.password)
+    return null
+  }
+}
