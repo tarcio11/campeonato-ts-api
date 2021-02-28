267abcde045867536e3a0473532094529629ed89
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-28 12:22:52 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -1,6 +1,6 @@
-import { Hasher } from '../protocols/cryptography/hasher'
-import { AddAccount } from '../../domain/usecases'
 import { DbAddAccount } from './db-add-account'
+import { AddAccount } from '../../domain/usecases'
+import { Hasher } from '../protocols/cryptography/hasher'
 
 import faker from 'faker'
 
@@ -10,18 +10,32 @@ const mockAddAccountParams = (): AddAccount.Params => ({
   password: faker.internet.password()
 })
 
+class HasherSpy implements Hasher {
+  digest = faker.random.uuid()
+  plaintext: string
+  async hash (plaintext: string): Promise<string> {
+    this.plaintext = plaintext
+    return this.digest
+  }
+}
+
+type SutTypes = {
+  sut: DbAddAccount
+  hasherSpy: HasherSpy
+}
+
+const makeSut = (): SutTypes => {
+  const hasherSpy = new HasherSpy()
+  const sut = new DbAddAccount(hasherSpy)
+  return {
+    sut,
+    hasherSpy
+  }
+}
+
 describe('DbAddAccount UseCase', () => {
   test('Should call Hasher with correct plaintext', async () => {
-    class HasherSpy implements Hasher {
-      digest = faker.random.uuid()
-      plaintext: string
-      async hash (plaintext: string): Promise<string> {
-        this.plaintext = plaintext
-        return this.digest
-      }
-    }
-    const hasherSpy = new HasherSpy()
-    const sut = new DbAddAccount(hasherSpy)
+    const { sut, hasherSpy } = makeSut()
     const addAccountParams = mockAddAccountParams()
     await sut.add(addAccountParams)
     expect(hasherSpy.plaintext).toBe(addAccountParams.password)
