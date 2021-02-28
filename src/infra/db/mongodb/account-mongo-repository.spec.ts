b0eb26376139cccf6a971cc670816d901d13e761
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-28 16:57:47 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -1,11 +1,14 @@
 import { AccountMongoRepository } from './account-mongo-repository'
 import { MongoHelper } from './mongo-helper'
 
+import { Collection } from 'mongodb'
 import faker from 'faker'
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository()
 }
+
+let accountCollection: Collection
 
 describe('AccountMongoRepository', () => {
   beforeAll(async () => {
@@ -14,6 +17,11 @@ describe('AccountMongoRepository', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   test('Should return an account on success', async () => {
