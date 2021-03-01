3c7566560c1ae671a1f25775b30e70e04b055ef1
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-28 21:29:40 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Tarcio Rocha',
+        email: 'tarcio@mail.com',
+        password: '123456'
+      })
+      .expect(200)
+  })
+})
