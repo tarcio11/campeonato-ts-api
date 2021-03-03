6a0578f0086edfe30e56167247d3cb0cb25c73d1
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-03 10:08:37 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -4,19 +4,33 @@ import { ok } from '../../presentation/helpers'
 
 import faker from 'faker'
 
+class ControllerSpy implements Controller {
+  request: any
+  httpResponse = ok(faker.random.uuid())
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    this.request = request
+    return this.httpResponse
+  }
+}
+
+type SutTypes = {
+  sut: LogControllerDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new LogControllerDecorator(controllerSpy)
+  return {
+    sut,
+    controllerSpy
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerSpy implements Controller {
-      request: any
-      httpResponse = ok(faker.random.uuid())
-
-      async handle (request: HttpRequest): Promise<HttpResponse> {
-        this.request = request
-        return this.httpResponse
-      }
-    }
-    const controllerSpy = new ControllerSpy()
-    const sut = new LogControllerDecorator(controllerSpy)
+    const { sut, controllerSpy } = makeSut()
     const request: any = faker.lorem.sentence()
     await sut.handle(request)
     expect(controllerSpy.request).toEqual(request)
