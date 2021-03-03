64bfa9efb700883f354a594f513af27d0ad46890
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-03 13:42:37 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login-controller.spec.ts
+++ src/presentation/controllers/login-controller.spec.ts
@@ -3,14 +3,25 @@ import { LoginController } from './login-controller'
 import faker from 'faker'
 import { badRequest } from '../helpers'
 import { MissingParamError } from '../errors'
+import { EmailValidatorSpy } from '../tests/mocks'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+type SutTypes = {
+  sut
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new LoginController(emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const request = {
       body: {
         password: faker.internet.password()
@@ -21,7 +32,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const request = {
       body: {
         email: faker.internet.email()
@@ -29,5 +40,17 @@ describe('Login Controller', () => {
     }
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should calls EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const request = {
+      body: {
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+    }
+    await sut.handle(request)
+    expect(emailValidatorSpy.email).toBe(request.body.email)
   })
 })
