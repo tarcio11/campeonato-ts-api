1735a1c5732a57189ccc841bc48b50eabc972568
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-03 13:19:03 -0300

    test: ensure LoginController returns 400 if no email is provider

---
+++ src/presentation/controllers/login-controller.spec.ts
@@ -0,0 +1,18 @@
+import { LoginController } from './login-controller'
+
+import faker from 'faker'
+import { badRequest } from '../helpers'
+import { MissingParamError } from '../errors'
+
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const request = {
+      body: {
+        password: faker.internet.password()
+      }
+    }
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
