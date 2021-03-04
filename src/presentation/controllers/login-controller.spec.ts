4d33cbaebf73666e811513ba2697fe5da18e57b0
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-04 11:20:50 -0300

    test: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login-controller.spec.ts
+++ src/presentation/controllers/login-controller.spec.ts
@@ -1,5 +1,6 @@
 import { LoginController } from './login-controller'
 import { HttpRequest } from '../protocols'
+import { Authentication } from '../../domain/usecases'
 import { InvalidParamError, MissingParamError, ServerError } from '../errors'
 import { EmailValidatorSpy } from '../tests/mocks'
 import { badRequest, serverError } from '../helpers'
@@ -7,26 +8,41 @@ import { badRequest, serverError } from '../helpers'
 import faker from 'faker'
 
 const mockRequest = (): HttpRequest => {
-  const email = faker.internet.email()
   return {
     body: {
-      email,
-      password: faker.internet.email()
+      email: faker.internet.email(),
+      password: faker.internet.password()
     }
+  }
+}
+
+class AuthenticationSpy implements Authentication {
+  params: Authentication.Params
+  result = {
+    accessToken: faker.random.uuid(),
+    name: faker.name.findName()
+  }
+
+  async auth (params: Authentication.Params): Promise<Authentication.Result> {
+    this.params = params
+    return this.result
   }
 }
 
 type SutTypes = {
   sut
   emailValidatorSpy: EmailValidatorSpy
+  authenticationSpy: AuthenticationSpy
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorSpy = new EmailValidatorSpy()
-  const sut = new LoginController(emailValidatorSpy)
+  const authenticationSpy = new AuthenticationSpy()
+  const sut = new LoginController(emailValidatorSpy, authenticationSpy)
   return {
     sut,
-    emailValidatorSpy
+    emailValidatorSpy,
+    authenticationSpy
   }
 }
 
@@ -72,5 +88,15 @@ describe('Login Controller', () => {
     jest.spyOn(emailValidatorSpy, 'isValid').mockImplementationOnce(() => { throw new Error() })
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should calls Authentication with correct values', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(authenticationSpy.params).toEqual({
+      email: request.body.email,
+      password: request.body.password
+    })
   })
 })
