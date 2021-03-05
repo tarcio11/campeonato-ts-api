32e2fe1ace83ddf969daf40f2b68de9b7f77690f
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-05 18:52:21 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-authentication.spec.ts
+++ src/data/usecases/db-authentication.spec.ts
@@ -2,10 +2,23 @@ import { DbAuthentication } from './db-authentication'
 import { LoadAccountByEmailRepositorySpy } from '../tests/mocks'
 import { mockAuthenticationParams } from '../../domain/tests/mocks'
 
+type SutTypes = {
+  sut: DbAuthentication
+  loadAccountByEmailRepositorySpy: LoadAccountByEmailRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositorySpy = new LoadAccountByEmailRepositorySpy()
+  const sut = new DbAuthentication(loadAccountByEmailRepositorySpy)
+  return {
+    sut,
+    loadAccountByEmailRepositorySpy
+  }
+}
+
 describe('DbAuthentication', () => {
   test('Should calls LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositorySpy = new LoadAccountByEmailRepositorySpy()
-    const sut = new DbAuthentication(loadAccountByEmailRepositorySpy)
+    const { sut, loadAccountByEmailRepositorySpy } = makeSut()
     const authenticationParams = mockAuthenticationParams()
     await sut.auth(authenticationParams)
     expect(loadAccountByEmailRepositorySpy.email).toBe(authenticationParams.email)
