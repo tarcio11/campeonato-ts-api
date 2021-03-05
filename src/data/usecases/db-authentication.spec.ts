8d9756d923a74c480be418ecb91072c160f00253
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-05 19:02:59 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/db-authentication.spec.ts
+++ src/data/usecases/db-authentication.spec.ts
@@ -30,4 +30,11 @@ describe('DbAuthentication', () => {
     const promise = sut.auth(mockAuthenticationParams())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositorySpy } = makeSut()
+    loadAccountByEmailRepositorySpy.result = null
+    const model = await sut.auth(mockAuthenticationParams())
+    expect(model).toBeNull()
+  })
 })
