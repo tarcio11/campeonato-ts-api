3317f3f4a9065c981a2649c089a933fd5c50106a
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-05 20:26:03 -0300

    test: ensure DbAuthentication returns an data on success

--- src/data/usecases/db-authentication.spec.ts
+++ src/data/usecases/db-authentication.spec.ts
@@ -78,4 +78,11 @@ describe('DbAuthentication', () => {
     const promise = sut.auth(mockAuthenticationParams())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an data on success', async () => {
+    const { sut, encrypterSpy, loadAccountByEmailRepositorySpy } = makeSut()
+    const { accessToken, name } = await sut.auth(mockAuthenticationParams())
+    expect(accessToken).toBe(encrypterSpy.ciphertext)
+    expect(name).toBe(loadAccountByEmailRepositorySpy.result.name)
+  })
 })
