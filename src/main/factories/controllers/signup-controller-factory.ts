fe65a198d231a0f7a770807bd096a1ce6d8e2100
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-05 11:13:11 -0300

    feat: ensure SignUpValidationFactory calls ValidationComposite with all validations

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -5,6 +5,7 @@ import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 import { BcryptAdapter } from '../../../infra/cryptography'
 import { AccountMongoRepository, LogMongoRepository } from '../../../infra/db/mongodb'
 import { LogControllerDecorator } from '../../decorators'
+import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -12,7 +13,7 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
