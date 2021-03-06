d7312208afaf64f8531c196712272a90b882f6c6
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-06 17:04:48 -0300

    feat: add new dependencie in SignUp Factory

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,17 +1,20 @@
 import { SignUpController } from '../../../presentation/controllers'
 import { Controller } from '../../../presentation/protocols'
-import { DbAddAccount } from '../../../data/usecases'
-import { BcryptAdapter } from '../../../infra/cryptography'
+import { DbAddAccount, DbAuthentication } from '../../../data/usecases'
+import { BcryptAdapter, JwtAdapter } from '../../../infra/cryptography'
 import { AccountMongoRepository, LogMongoRepository } from '../../../infra/db/mongodb'
 import { LogControllerDecorator } from '../../decorators'
 import { makeSignUpValidation } from './signup-validation-factory'
+import env from '../../config/env'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), dbAuthentication)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
