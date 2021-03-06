98029a77102d44d13250a70858faf293e5890083
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-06 15:36:39 -0300

    feat: ensure DbAddAccount return false if CheckAccountByEmailRepository returns true

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -9,9 +9,12 @@ export class DbAddAccount implements AddAccount {
   ) {}
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
-    await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    const isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    const exist = await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
+    let isValid = false
+    if (!exist) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    }
     return isValid
   }
 }
