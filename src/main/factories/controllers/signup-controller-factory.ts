e185a15d7830dfee7b2137a9280aeafb2da9f045
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-06 15:51:47 -0300

    refactor: add dependencie correct

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -5,14 +5,12 @@ import { BcryptAdapter } from '../../../infra/cryptography'
 import { AccountMongoRepository, LogMongoRepository } from '../../../infra/db/mongodb'
 import { LogControllerDecorator } from '../../decorators'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { CheckAccountByEmailRepositorySpy } from '../../../data/tests/mocks'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository, checkAccountByEmailRepositorySpy)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
   const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
