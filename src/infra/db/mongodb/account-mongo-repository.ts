e35c10a3e9f7cc53d1ab62d5cf35c80d0e927d0a
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-06 12:32:00 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,9 +1,23 @@
-import { AddAccountRepository } from '../../../data/protocols'
+import { AddAccountRepository, LoadAccountByEmailRepository } from '../../../data/protocols'
 import { MongoHelper } from './mongo-helper'
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = accountCollection.insertOne(data)
     return (await result).ops[0] !== null
+  }
+
+  async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({
+      email
+    }, {
+      projection: {
+        _id: 1,
+        name: 1,
+        password: 1
+      }
+    })
+    return account && MongoHelper.map(account)
   }
 }
