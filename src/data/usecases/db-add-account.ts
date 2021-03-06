86f09283f1dc75a206f68a88e6e681a4d1a88df8
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-06 15:24:07 -0300

    feat: ensure DbAddAccount calls CheckAccountByEmailRepository with correct email

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,13 +1,15 @@
 import { AddAccount } from '../../domain/usecases'
-import { AddAccountRepository, Hasher } from '../protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository, Hasher } from '../protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
   ) {}
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
     const hashedPassword = await this.hasher.hash(accountData.password)
     const isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
     return isValid
--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -5,12 +5,14 @@ import { BcryptAdapter } from '../../../infra/cryptography'
 import { AccountMongoRepository, LogMongoRepository } from '../../../infra/db/mongodb'
 import { LogControllerDecorator } from '../../decorators'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { CheckAccountByEmailRepositorySpy } from '../../../data/tests/mocks'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository, checkAccountByEmailRepositorySpy)
   const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
