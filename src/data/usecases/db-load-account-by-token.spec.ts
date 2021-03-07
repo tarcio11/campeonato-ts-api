6f74796cbfbabb502b532bbfa5f7258263f03325
Tarcio Rocha <tarcio_x@hotmail.com>
2021-03-07 11:55:08 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-load-account-by-token.spec.ts
+++ src/data/usecases/db-load-account-by-token.spec.ts
@@ -3,12 +3,31 @@ import { DecrypterSpy } from '../tests/mocks'
 
 import faker from 'faker'
 
+type SutTypes = {
+  sut: DbLoadAccountByToken
+  decrypterSpy: DecrypterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterSpy = new DecrypterSpy()
+  const sut = new DbLoadAccountByToken(decrypterSpy)
+  return {
+    sut,
+    decrypterSpy
+  }
+}
+
+let token: string
+let role: string
+
 describe('DbLoadAccountByToken UseCase', () => {
+  beforeEach(() => {
+    token = faker.random.uuid()
+    role = faker.random.word()
+  })
+
   test('Should call Decrypter with correct ciphertext', async () => {
-    const decrypterSpy = new DecrypterSpy()
-    const sut = new DbLoadAccountByToken(decrypterSpy)
-    const token = faker.random.uuid()
-    const role = faker.random.word()
+    const { sut, decrypterSpy } = makeSut()
     await sut.load(token, role)
     expect(decrypterSpy.ciphertext).toBe(token)
   })
