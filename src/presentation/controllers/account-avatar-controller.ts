bd31e83d5e2f4912e7f200e6e687792a0e84fd69
tarcio11 <tarcio_x@hotmail.com>
2021-06-02 14:59:00 -0300

    feat: ensure AccountAvatarController call UpdateAvatar with correct values

--- src/presentation/controllers/account-avatar-controller.ts
+++ src/presentation/controllers/account-avatar-controller.ts
@@ -1,20 +1,26 @@
 import { Controller, HttpResponse, Validation } from '../protocols'
 import { badRequest } from '../helpers'
+import { UpdateAvatar } from '../../domain/usecases'
 
 export class AccountAvatarController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly updateAvatar: UpdateAvatar
+  ) {}
 
   async handle (request: UpdateAvatarController.Request): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+    await this.updateAvatar.update(request)
     return null
   }
 }
 
 export namespace UpdateAvatarController {
   export type Request = {
+    accountId: string
     name: string
   }
 }
