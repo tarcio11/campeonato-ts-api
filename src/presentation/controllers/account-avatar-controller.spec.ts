b1d60940785b9a0e374289826d66a36eaf9eaaf7
tarcio11 <tarcio_x@hotmail.com>
2021-06-02 12:25:39 -0300

    test: ensure AccountAvatarController return 400 if Validation returns an error

--- src/presentation/controllers/account-avatar-controller.spec.ts
+++ src/presentation/controllers/account-avatar-controller.spec.ts
@@ -1,5 +1,7 @@
 import { AccountAvatarController } from './account-avatar-controller'
 import { ValidationSpy } from '../tests/mocks'
+import { badRequest } from '../helpers'
+import { MissingParamError } from '../errors'
 
 import faker from 'faker'
 
@@ -25,5 +27,15 @@ describe('AccountAvatarController', () => {
     }
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new MissingParamError(faker.random.word())
+    const request = {
+      name: faker.internet.url()
+    }
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
