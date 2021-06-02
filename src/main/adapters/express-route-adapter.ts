a4ab87f21d227013f9b20a7611ad94dbd33ae657
tarcio11 <tarcio_x@hotmail.com>
2021-06-02 16:18:55 -0300

    chore: add multer and configs

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -3,8 +3,22 @@ import { Controller } from '../../presentation/protocols'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
+    const { file } = req
+    let mappedFiles = {}
+    if (file) {
+      mappedFiles = ((file as Express.Multer.File), {
+        name: file.originalname,
+        type: file.mimetype,
+        content: file.buffer,
+        size: file.size,
+        extension: `${file.originalname.split('.').pop()}`
+      })
+    }
+
     const request = {
-      ...(req.body || {})
+      ...(req.body || {}),
+      ...(mappedFiles || {}),
+      accountId: req.accountId
     }
     const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode === 200) {
