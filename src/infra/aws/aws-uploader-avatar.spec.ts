49f599520f5482a6879079759d65fced65d237b8
tarcio11 <tarcio_x@hotmail.com>
2021-06-02 11:16:48 -0300

    refactor: move code to private helper method

--- src/infra/aws/aws-uploader-avatar.spec.ts
+++ src/infra/aws/aws-uploader-avatar.spec.ts
@@ -2,6 +2,7 @@ import { AwsUploaderAvatar } from './aws-uploader-avatar'
 import { mockUploadAvatarParams } from '../../domain/tests/mocks'
 
 import { S3 } from 'aws-sdk'
+import faker from 'faker'
 
 S3.prototype.putObject = jest.fn().mockImplementation(() => ({
   promise: async () => await Promise.resolve()
@@ -13,19 +14,26 @@ jest.mock('../../main/config/env', () => ({
   defaultFilesACL: 'private'
 }))
 
+const mockDate = (): number => {
+  const timestamp = faker.random.number()
+  global.Date.now = jest.fn().mockReturnValueOnce(timestamp)
+  return timestamp
+}
+
 const makeSut = (): AwsUploaderAvatar => new AwsUploaderAvatar()
 
 describe('AwsUploaderAvatar', () => {
-  test('should call putObject for a single file', async () => {
+  test('should call putObject with correct options', async () => {
     const sut = makeSut()
     const putObjectSpy = jest.spyOn(S3.prototype, 'putObject')
     const uploadAvatarParams = mockUploadAvatarParams()
+    const timestamp = mockDate()
     await sut.upload(uploadAvatarParams)
     expect(putObjectSpy).toHaveBeenCalledWith({
       Bucket: 'aws-bucket',
       ContentType: uploadAvatarParams.type,
       Body: uploadAvatarParams.content,
-      Key: uploadAvatarParams.name,
+      Key: `${uploadAvatarParams.name}-${timestamp}.${uploadAvatarParams.extension}`,
       ACL: 'private'
     })
   })
--- src/infra/aws/aws-uploader-avatar.ts
+++ src/infra/aws/aws-uploader-avatar.ts
@@ -13,17 +13,30 @@ export class AwsUploaderAvatar implements UploadedAvatar {
     })
   }
 
-  async upload (file: UploadedAvatar.Params): Promise<UploadedAvatar.Result> {
+  private generateFileKey (file: UploadedAvatar.Params, timestamp: number): string {
+    return `${file.name}-${timestamp}.${file.extension}`
+  }
+
+  private async uploadFile (file: UploadedAvatar.Params): Promise<UploadedAvatar.Result> {
+    const timestamp = Date.now()
+    const fileKey = this.generateFileKey(file, timestamp)
     await this.client
       .putObject({
         Bucket: this.bucketName,
-        Key: file.name,
+        Key: fileKey,
         ContentType: file.type,
         Body: file.content,
         ACL: env.defaultFilesACL
       })
       .promise()
 
+    return {
+      avatar_url: `${this.bucketName}${fileKey}`
+    }
+  }
+
+  async upload (file: UploadedAvatar.Params): Promise<UploadedAvatar.Result> {
+    await this.uploadFile(file)
     return null
   }
 }
