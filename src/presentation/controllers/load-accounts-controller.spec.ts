4f24855552846961998ad073302987fd80c577df
tarcio11 <tarcio_x@hotmail.com>
2021-06-03 17:43:36 -0300

    test: ensure LoadAccountsController return 500 if LoadAccounts throws

--- src/presentation/controllers/load-accounts-controller.spec.ts
+++ src/presentation/controllers/load-accounts-controller.spec.ts
@@ -1,6 +1,6 @@
 import { LoadAccountsController } from './load-accounts-controller'
 import { LoadAccountsSpy } from '../tests/mocks'
-import { noContent, ok } from '../helpers'
+import { noContent, ok, serverError } from '../helpers'
 
 type SutTypes = {
   sut: LoadAccountsController
@@ -34,5 +34,12 @@ describe('LoadAccounts Controller', () => {
     loadAccountsSpy.result = []
     const httpResponse = await sut.handle()
     expect(httpResponse).toEqual(noContent())
+  })
+
+  test('Should return 500 if LoadAccounts throws', async () => {
+    const { sut, loadAccountsSpy } = makeSut()
+    jest.spyOn(loadAccountsSpy, 'load').mockImplementationOnce(() => { throw new Error() })
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
