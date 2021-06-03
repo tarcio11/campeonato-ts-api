2c8b28a75f01bac25396db406a893fc3343a073a
tarcio11 <tarcio_x@hotmail.com>
2021-06-03 17:56:31 -0300

    feat: ensure users routes return 403 on load accounts result without accessToken

--- src/main/routes/users-routes.ts
+++ src/main/routes/users-routes.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '../adapters'
-import { makeAccountAvatarController } from '../factories/controllers'
+import { makeAccountAvatarController, makeLoadAccountsController } from '../factories/controllers'
 import { auth } from '../middlewares'
 
 import { Router } from 'express'
@@ -7,5 +7,7 @@ import multer from 'multer'
 const uploadConfig = multer()
 
 export default (router: Router): void => {
+  router.get('/users', auth, adaptRoute(makeLoadAccountsController()))
+
   router.patch('/users/avatar', auth, uploadConfig.single('avatar'), adaptRoute(makeAccountAvatarController()))
 }
