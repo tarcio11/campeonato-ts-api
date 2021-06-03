65f5a233705c14f622c44773a3237ca3aab7c63d
tarcio11 <tarcio_x@hotmail.com>
2021-06-03 17:42:42 -0300

    feat: ensure LoadAccountsController return 500 if LoadAccounts throws

--- src/presentation/controllers/load-accounts-controller.ts
+++ src/presentation/controllers/load-accounts-controller.ts
@@ -1,12 +1,16 @@
 import { Controller, HttpResponse } from '../protocols'
 import { LoadAccounts } from '../../domain/usecases'
-import { noContent, ok } from '../helpers'
+import { noContent, ok, serverError } from '../helpers'
 
 export class LoadAccountsController implements Controller {
   constructor (private readonly loadAccounts: LoadAccounts) {}
 
   async handle (): Promise<HttpResponse> {
-    const accountModels = await this.loadAccounts.load()
-    return accountModels.length ? ok(accountModels) : noContent()
+    try {
+      const accountModels = await this.loadAccounts.load()
+      return accountModels.length ? ok(accountModels) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
